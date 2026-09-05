7c9e6409d78560cfdda8d70712f157d6f63c1bd9
EnriqueJ97 <enriquejimenezcruz@gmail.com>
2026-09-05 15:06:27 +0200

    chore: support preview base path outside Lovable

--- src/App.tsx
+++ src/App.tsx
@@ -393,7 +393,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <AppRoutes />
         </BrowserRouter>
       </TooltipProvider>
